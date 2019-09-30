99773f16362e33c4a0b2cf3f8e0f94b1aea510ce
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-09-30 18:56:46 +0200

    Initial Commit

---
+++ Baluchon/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Baluchon
+//
+//  Created by Fabrice Etiennette on 30/09/2019.
+//  Copyright © 2019 Fabrice Etiennette. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
