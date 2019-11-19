b3cd6ac33e1fab62b04409fe7ca7095d87552a4b
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-11-19 18:59:44 +0100

    add tab bar view controller

---
+++ Baluchon/scene/TabBar/TabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarViewController.swift
+//  Baluchon
+//
+//  Created by Fabrice Etiennette on 19/11/2019.
+//  Copyright © 2019 Fabrice Etiennette. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
