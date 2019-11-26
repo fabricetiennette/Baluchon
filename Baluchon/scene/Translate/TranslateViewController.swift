07007b478fe87aed6f0c09bb8853a2eb110d935c
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-11-26 14:28:01 +0100

    translate logic done

--- Baluchon/scene/Translate/TranslateViewController.swift
+++ Baluchon/scene/Translate/TranslateViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class TranslateViewController: UIViewController {
 
+    @IBOutlet weak var toTranslateTextField: UITextView!
+    @IBOutlet weak var translationResultTextView: UITextView!
+    @IBOutlet weak var translateButton: UIButton!
+    @IBOutlet weak var exchangeLanguage: UIButton!
+    @IBOutlet weak var frLanguage: UILabel!
+    @IBOutlet weak var enLanguage: UILabel!
+
+    var translate = Translate()
+    var index: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction private func changeLanguage(_ sender: UIButton) {
+        sender.rotateIt()
+        (frLanguage.text, enLanguage.text) = (enLanguage.text, frLanguage.text)
+        if frLanguage.text == "Fr" {
+            self.index = 0
+        } else {
+            self.index = 1
+        }
+    }
+
+    @IBAction func didTapeTranslateButton(_ sender: Any) {
+        guard toTranslateTextField.text != "" else {
+            alert(title: "Error", message: "Text Missing")
+            return
+        }
+
+        translate.translate(index: index, text: toTranslateTextField.text) { (success, translatedText) in
+            if success == true {
+                self.refreshScreen(text: translatedText!, textView: self.translationResultTextView)
+            } else {
+                self.alert(title: "Error", message: "Web service problem")
+            }
+        }
     }
 }
