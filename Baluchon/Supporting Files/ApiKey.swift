a32a2c8d2e28fb9461fd9d0bbf27bd4ade77d570
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-11-29 17:47:08 +0100

    new commit

---
+++ Baluchon/Supporting Files/ApiKey.swift
@@ -0,0 +1,16 @@
+//
+//  ApiKey.swift
+//  Baluchon
+//
+//  Created by Fabrice Etiennette on 27/11/2019.
+//  Copyright © 2019 Fabrice Etiennette. All rights reserved.
+//
+
+import Foundation
+
+func valueForAPIKey(named keyname: String) -> AnyObject {
+  let filePath = Bundle.main.path(forResource: "ApiKeys", ofType: "plist")
+  let plist = NSDictionary(contentsOfFile: filePath!)
+    let value = (plist?.object(forKey: keyname))! as AnyObject
+  return value
+}
---
+++ Podfile
@@ -0,0 +1,19 @@
+
+target 'Baluchon' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Baluchon
+  pod 'SwiftLint'
+  pod 'SwiftGen', '~> 6.0'
+
+  target 'BaluchonTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'BaluchonUITests' do
+    # Pods for testing
+  end
+
+end
